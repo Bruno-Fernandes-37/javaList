697253380065aa53070e259727362270074aa188
bbrowny <bruno.fernandes3789@gmail.com>
2023-06-29 14:48:09 +0200

    first commit

---
+++ Thanos.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Thanos {
+
+    public static void main(String[] args) {
+
+        // TODO 1 : Create an empty heroes list
+        ArrayList<Hero> heroes = new ArrayList<>();
+
+        // TODO 2 : Add those heroes to the list
+        Hero blackWidow = new Hero("Black Widow", 34);
+        Hero captainAmericanew = new Hero("Captain America", 100);
+        Hero vision = new Hero("Vision", 3);
+        Hero ironMan = new Hero("Iron Man", 48);
+        Hero scarletWitch = new Hero("Scarlet Witch", 29);
+        Hero thor = new Hero("Thor", 1500);
+        Hero hulk = new Hero("Hulk", 49);
+        Hero doctorStrange = new Hero("Doctor Strange", 42);
+        heroes.add(blackWidow);
+        heroes.add(captainAmericanew);
+        heroes.add(vision);
+        heroes.add(ironMan);
+        heroes.add(scarletWitch);
+        heroes.add(thor);
+        heroes.add(hulk);
+        heroes.add(doctorStrange);
+
+        // TODO 3 : It's Thor birthday, now he's 1501
+        thor.setAge(1501);
+
+        // TODO 4 : Shuffle the heroes list
+        Collections.shuffle(heroes);
+
+        // TODO 5 : Keep only the half of the list
+        var demisHeroes = heroes.subList(0,heroes.size()/2);
+
+        // TODO 6 : Loop throught the list and display the name of the remaining heroes
+        for (Hero hero : demisHeroes) {
+            System.out.println(hero.getName());
+        }
+    }
+}
